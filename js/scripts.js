12741db982c49147aade15a9ae10630d2f08f892
kterry779 <kterry1@eagles.ewu.edu>
2020-01-29 14:59:03 -0800

    fix and finish day two

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 console.log('OK');
 
-const player = new Plyr('#player');
+const player = new Plyr('#player', {
+  
+    // Turn on/off conrols. Array data type
+    controls : [ 'play', 'current-time', 'progress' ],
+    
+
+  
+});
